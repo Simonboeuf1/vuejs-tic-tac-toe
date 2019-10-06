d664dcd81f082869b84df8286ae6926f6865f4eb
Simon Boeuf <sboeuf@xebia.fr>
2019-10-07 00:52:32 +0200

    improve code for xke hands on

--- src/utils/board.js
+++ src/utils/board.js
@@ -1,19 +1,11 @@
 import {Players, Markers, BOARD_SIZE} from "../constants";
 
-export const mapPlayerToMarker = new Map([
+const mapPlayerToMarker = new Map([
   [Players.Player1, Markers.Marker1],
   [Players.Player2, Markers.Marker2]
 ]);
 
-export const findTile = (board, x, y) => board.find((tile) => tile.x === x && tile.y == y);
-
-export const getMarker = (player) => mapPlayerToMarker.get(player);
-
-export const isTileEmpty = ({value}) => {
-  return value.length === 0;
-}
-
-export const isAllEq = (arr) => {
+const isAllEq = (arr) => {
   if(arr.length === 0) {
     return false;
   }
@@ -21,26 +13,37 @@ export const isAllEq = (arr) => {
   return !arr.some(val => val !== firstValue);
 }
 
-export const isFilledWithSameValue = (tiles) => tiles.length === BOARD_SIZE && isAllEq(tiles.map(({value}) => value));
+const isFilledWithSameValue = (tiles) =>
+  tiles.length === BOARD_SIZE && isAllEq(tiles.map(({value}) => value));
 
-export const checkLine = (tiles) => {
+const checkLine = (tiles) => {
   const indexes = Array(BOARD_SIZE).fill(null);
-  return indexes.some((_, index) => isFilledWithSameValue(tiles.filter(tile => tile.number === index)))
+  return indexes.some(
+    (_, index) => isFilledWithSameValue(tiles.filter(tile => tile.number === index))
+  )
 }
 
-export const checkRows = (board) => checkLine(board.map(({x, value}) => ({number: x, value})));
+const checkRows = (board) => checkLine(board.map(({x, value}) => ({number: x, value})));
 
-export const checkCols = (board) => checkLine(board.map(({y, value}) => ({number: y, value})));
+const checkCols = (board) => checkLine(board.map(({y, value}) => ({number: y, value})));
 
-export const checkDiags = (board) => {
+const checkDiags = (board) => {
   const firstDiag = board.filter(tile => tile.x === tile.y);
   const secondDiag = board.filter(tile => tile.x + tile.y === BOARD_SIZE - 1);
+
   return isFilledWithSameValue(firstDiag) || isFilledWithSameValue(secondDiag);
 }
 
 export const hasWinner = (board) => {
   const filledTiles = board.filter(tile => tile.value.length !== 0)
+
   return checkRows(filledTiles) || checkCols(filledTiles) || checkDiags(filledTiles);
 }
 
 export const hasSpaceLeft = (board) => board.some(tile => tile.value.length === 0);
+
+export const getMarker = (player) => mapPlayerToMarker.get(player);
+
+export const isTileEmpty = ({value}) => value.length === 0;
+
+export const isGameFinished = (board) => !hasSpaceLeft(board) || hasWinner(board);
--- src/utils/game.js
+++ src/utils/game.js
@@ -1,13 +1,14 @@
 import {Players} from "../constants";
-import {getMarker, hasWinner, hasSpaceLeft} from "./board";
+import {getMarker} from "./board";
 
 
 export const markTile = (board, tile, player) => {
   const marker = getMarker(player);
-  const tileIndex = board.findIndex(t => t === tile);
-  return [...board.slice(0, tileIndex), ...board.slice(tileIndex + 1, board.length), {...tile, value: marker}];
+  const tileIndex = board.findIndex(t => t.x === tile.x && t.y === tile.y);
+  const newTile = {...tile, value: marker};
+  const boardWithoutTile = [...board.slice(0, tileIndex), ...board.slice(tileIndex + 1)];
+
+  return [...boardWithoutTile, newTile];
 }
 
-export const switchPlayer = (player) => player === Players.Player1 ? Players.Player2 : Players.Player1
-
-export const isGameFinished = (board) => !hasSpaceLeft(board) || hasWinner(board);
+export const getNextPlayer = (player) => player === Players.Player1 ? Players.Player2 : Players.Player1
